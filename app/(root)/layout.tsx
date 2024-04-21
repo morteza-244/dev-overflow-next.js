92e2105185fab3b4cb8912c71c1fb05b2ddf8b69
morteza <mori2002sa@gmail.com>
2024-04-21 16:12:06 +0330

    Implement home page layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from "react";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="relative bg-light-850 dark:bg-dark-100">
+      Navbar
+      <div className="flex">
+        RightSidebar
+        <section className="min-h-screen flex flex-col-flex-1 px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="w-full mx-auto max-w-5xl">{children}</div>
+        </section>
+        LefSidebar
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
