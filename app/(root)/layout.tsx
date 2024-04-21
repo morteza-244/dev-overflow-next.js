089e2fb5759b620b091d5577c7e08888942a2cb5
morteza <mori2002sa@gmail.com>
2024-04-21 16:48:54 +0330

    Build the navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
+import NavBar from "@/components/shared/navbar/NavBar";
 import { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <main className="relative bg-light-850 dark:bg-dark-100">
-      Navbar
+      <NavBar />
       <div className="flex">
         RightSidebar
         <section className="min-h-screen flex flex-col-flex-1 px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
---
+++ components/shared/navbar/NavBar.tsx
@@ -0,0 +1,38 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between items-center bg-[#ffffff] dark:bg-[#0F1117] fixed z-50 w-full gap-5 p-6 shadow-lg dark:shadow-none sm:px-12">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src={"/assets/images/site-logo.svg"}
+          alt="Dev-Overflow"
+          width={23}
+          height={23}
+        />
+        <p className="text-[24px] font-semibold leading-[31.2px] text-[#000000] dark:text-[#FFFFFF] max-sm:hidden">
+          Dev <span className="text-orange-500">Overflow</span>
+        </p>
+      </Link>
+      <div className="flex justify-between items-center gap-5">
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
