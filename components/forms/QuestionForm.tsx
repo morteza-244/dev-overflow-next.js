4ba37c07bd6561bd9a0d91a14a1d1b47ee872f1e
morteza <mori2002sa@gmail.com>
2024-04-23 20:08:07 +0330

    Show Tiny Editor error message

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -13,8 +13,10 @@ import { Input } from "@/components/ui/input";
 import { questionFormSchema, TQuestionFormData } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
-import { useRef } from "react";
+import { X } from "lucide-react";
+import { KeyboardEvent, useRef } from "react";
 import { useForm } from "react-hook-form";
+import { Badge } from "../ui/badge";
 const QuestionForm = () => {
   const editorRef = useRef(null);
   const form = useForm<TQuestionFormData>({
@@ -25,14 +27,45 @@ const QuestionForm = () => {
       tags: [],
     },
   });
-  // const log = () => {
-  //   if (editorRef.current) {
-  //     console.log(editorRef.current.getContent());
-  //   }
-  // };
+  
   const onSubmit = (data: TQuestionFormData) => {
     console.log(data);
   };
+
+  const handleInputKeyDown = (
+    event: KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (event.key === "Enter" && field.name === "tags") {
+      event.preventDefault();
+
+      const tagInput = event.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== " ") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters.",
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
+
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -66,10 +99,13 @@ const QuestionForm = () => {
                     // @ts-ignore
                     editorRef.current = editor;
                   }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
                   initialValue=""
                   init={{
                     height: 350,
                     menubar: false,
+                    browser_spellcheck: true,
                     plugins: [
                       "advlist",
                       "autolink",
@@ -81,27 +117,23 @@ const QuestionForm = () => {
                       "anchor",
                       "searchreplace",
                       "visualblocks",
-                      "code",
+                      "codesample",
                       "fullscreen",
                       "insertdatetime",
                       "media",
                       "table",
-                      "codesample",
-                      "help",
                     ],
                     toolbar:
-                      "undo redo | blocks | " +
+                      "undo redo | " +
                       "codesample | bold italic forecolor | alignleft aligncenter |" +
-                      "alignright alignjustify | bullist numlist ",
-                    content_style:
-                      "body { font-family:Helvetica,Arial,sans-serif; font-size:16px }",
+                      "alignright alignjustify | bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
                   }}
-                  {...field}
                 />
               </FormControl>
               <FormDescription>
-                Be specific and imagine you're asking a question to another
-                person.
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
               </FormDescription>
               <FormMessage className="text-red-600" />
             </FormItem>
@@ -114,7 +146,26 @@ const QuestionForm = () => {
             <FormItem>
               <FormLabel>Tags</FormLabel>
               <FormControl>
-                <Input placeholder="Add tags..." {...field} />
+                <>
+                  <Input
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex gap-2 flex-wrap">
+                      {field.value.map((tag) => (
+                        <Badge className="bg-slate-200 text-slate-600 dark:bg-muted dark:text-slate-400 flex-center gap-1 rounded-md border-none py-2 capitalize active:scale-95 transition-all">
+                          {tag}
+                          <X
+                            size={15}
+                            onClick={() => handleTagRemove(tag, field)}
+                            className="cursor-pointer"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription>
                 Add up to 3 tags to describe what your question is about. You
