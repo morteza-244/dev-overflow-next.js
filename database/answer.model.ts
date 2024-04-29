cf2418a76b7edc7b8f94ec2e3ac1d805fcc7b836
morteza <mori2002sa@gmail.com>
2024-04-29 12:06:05 +0330

    Create Answer model

---
+++ database/answer.model.ts
@@ -0,0 +1,21 @@
+import { Document, model, Schema } from "mongoose";
+
+export interface IAnswer extends Document {
+  author: Schema.Types.ObjectId;
+  question: Schema.Types.ObjectId;
+  upVotes: Schema.Types.ObjectId[];
+  downVotes: Schema.Types.ObjectId[];
+  content: string;
+  createdAt: Date;
+}
+
+const AnswerSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  upVotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downVotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Answer = model("Answer", AnswerSchema);
+export default Answer;
