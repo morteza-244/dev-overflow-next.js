61ac5c551417c6b2e5578f8c53ad1b5e95ac3aab
morteza <mori2002sa@gmail.com>
2024-05-04 14:33:01 +0330

    Delete questions and answers

--- components/shared/ActionsButton.tsx
+++ components/shared/ActionsButton.tsx
@@ -1,28 +1,69 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { deleteAnswer } from "@/lib/actions/answer.action";
+import { deleteQuestion } from "@/lib/actions/question.action";
 import { TActions } from "@/types";
 import { Pencil, Trash2 } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import SubmitLoading from "./SubmitLoading";
 
 interface ActionsButtonProps {
   type: TActions;
   itemId: string;
 }
 
-const ActionsButton = ({ type }: ActionsButtonProps) => {
-  const handleDelete = () => {};
+const ActionsButton = ({ type, itemId }: ActionsButtonProps) => {
+  const pathname = usePathname();
+  const [isPending, setIsPending] = useState(false);
+  const handleDelete = async () => {
+    try {
+      setIsPending(true);
+      if (type === "QUESTION") {
+        await deleteQuestion({
+          questionId: itemId,
+          path: pathname,
+        });
+      } else {
+        await deleteAnswer({
+          answerId: itemId,
+          path: pathname,
+        });
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
   const handleEdit = () => {};
   return (
     <div className="flex items-center justify-end gap-3 w-full">
       {type === "QUESTION" && (
-        <Button size={"sm"} variant={"secondary"} onClick={handleEdit} className="w-full xs:w-auto">
+        <Button
+          size={"sm"}
+          variant={"secondary"}
+          onClick={handleEdit}
+          className="w-full xs:w-auto"
+        >
           <Pencil size={17} className="text-gray-500 mr-2" />
           Edit
         </Button>
       )}
-      <Button size={"sm"} variant={"secondary"} onClick={handleDelete} className="w-full xs:w-auto">
-        <Trash2 size={17} className="text-red-600 mr-2" />
-        Delete
+      <Button
+        size={"sm"}
+        variant={"secondary"}
+        onClick={handleDelete}
+        className="w-full xs:w-auto"
+        disabled={isPending}
+      >
+        {isPending ? (
+          <SubmitLoading label="Wait" />
+        ) : (
+          <>
+            <Trash2 size={17} className="text-red-600 mr-2" />
+            Delete
+          </>
+        )}
       </Button>
     </div>
   );
