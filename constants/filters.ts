3167bbda817388d83561c06c37e8a939db94b26d
morteza <mori2002sa@gmail.com>
2024-05-12 17:58:47 +0330

    Remove recommended option from filter selector

--- constants/filters.ts
+++ constants/filters.ts
@@ -1,6 +1,5 @@
 export const homePageFilters = [
   { name: "Newest", value: "newest" },
-  { name: "Recommended", value: "recommended" },
   { name: "Frequent", value: "frequent" },
   { name: "Unanswered", value: "unanswered" },
 ];
